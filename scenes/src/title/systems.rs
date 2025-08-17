c8a5a83cedd177876174f9760ba619ba8c7821ba
Tokusei Noborio <tokusei@nyampass.com>
2025-08-17 22:14:55 +0900

    show title

--- scenes/src/title/systems.rs
+++ scenes/src/title/systems.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::color::palettes::css::*;
 
 use super::components::TitleUI;
 use crate::assets::FontKey;
@@ -12,28 +13,50 @@ pub fn setup(
 ) {
     clear_color.0 = Color::WHITE;
 
-    commands.entity(ui_root.0).with_children(|parent| {
+ commands.entity(ui_root.0).with_children(|parent| {
         parent
-            .spawn((
-                Node {
-                    width: Val::Percent(100.0),
-                    top: Val::Percent(10.0),
-                    position_type: PositionType::Absolute,
-                    justify_content: JustifyContent::Center,
-                    ..default()
-                },
-                TitleUI,
-            ))
-            .with_children(|p| {
-                p.spawn((
-                    Text::new("keystone"),
-                    TextFont {
-                        font: asset_store.font(FontKey::Title).unwrap(),
-                        font_size: 32.0,
+            .spawn(Node {
+                width: Val::Percent(100.0),
+                height: Val::Px(290.0),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            })
+            .with_children(|center| {
+                center
+                    .spawn(Node {
                         ..default()
-                    },
-                    TextColor(Color::BLACK),
-                ));
+                    })
+                    .with_children(|stack| {
+                        stack.spawn((
+                            Node {
+                                position_type: PositionType::Absolute,
+                                left: Val::Px(2.0),
+                                top: Val::Px(2.0),
+                                ..default()
+                            },
+                            Text::new("KEYSTONE: CALL OF CATS"),
+                            TextFont {
+                                font: asset_store.font(FontKey::Title).unwrap(),
+                                font_size: 40.0,
+                                ..default()
+                            },
+                            TextColor(Color::srgb(0.8, 0.333, 0.0)),
+                            TitleUI,
+                        ));
+
+                        stack.spawn((
+                            // ZIndex::Local(1),
+                            Text::new("KEYSTONE: CALL OF CATS"),
+                            TextFont {
+                                font: asset_store.font(FontKey::Title).unwrap(),
+                                font_size: 40.0,
+                                ..default()
+                            },
+                            TextColor(Color::from(ORANGE)),
+                            TitleUI,
+                        ));
+                    });
             });
     });
 }
