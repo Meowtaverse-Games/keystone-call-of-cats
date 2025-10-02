13965cb0befd1e67f28d090a6359de1a5d2fba14
Tokusei Noborio <tokusei@nyampass.com>
2025-10-02 19:49:04 +0900

    Refactor TitlePlugin build method: streamline app system addition

--- src/scenes/title/mod.rs
+++ src/scenes/title/mod.rs
@@ -8,8 +8,7 @@ mod systems;
 pub struct TitlePlugin;
 impl Plugin for TitlePlugin {
     fn build(&self, app: &mut App) {
-        app
-            .add_systems(OnEnter(GameState::Title), systems::setup)
+        app.add_systems(OnEnter(GameState::Title), systems::setup)
             .add_systems(OnExit(GameState::Title), systems::cleanup);
     }
 }
