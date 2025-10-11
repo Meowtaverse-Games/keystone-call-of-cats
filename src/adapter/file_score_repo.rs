7f71878a2d3f3cc289a39390e3db4318ae18eabb
Tokusei Noborio <tokusei@nyampass.com>
2025-10-11 15:47:47 +0900

    Remove commented-out score repository, boundary traits, and player struct code

--- src/adapter/file_score_repo.rs
+++ src/adapter/file_score_repo.rs
@@ -1,18 +1,18 @@
-use bevy::prelude::Resource;
+// use bevy::prelude::Resource;
 
-use crate::core::boundary::ScoreRepo;
+// use crate::core::boundary::ScoreRepo;
 
-#[derive(Resource)]
-pub struct FileScoreRepo;
+// #[derive(Resource)]
+// pub struct FileScoreRepo;
 
-impl ScoreRepo for FileScoreRepo {
-    fn save(&self, score: u32) {
-        // TODO: Implement file save
-        println!("Saving score: {}", score);
-    }
+// impl ScoreRepo for FileScoreRepo {
+//     fn save(&self, score: u32) {
+//         // TODO: Implement file save
+//         println!("Saving score: {}", score);
+//     }
 
-    fn load(&self) -> u32 {
-        // TODO: Implement file load
-        0
-    }
-}
+//     fn load(&self) -> u32 {
+//         // TODO: Implement file load
+//         0
+//     }
+// }
