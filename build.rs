f3785362c8184381e1b758c87ef12bd03d512af0
Tokusei Noborio <tokusei@nyampass.com>
2025-12-28 13:42:08 +0900

    refactor: Migrate from `bevy_embedded_assets` to direct asset copying, update `bevy_asset_loader`, and integrate `sccache` in Windows CI.

--- build.rs
+++ build.rs
@@ -2,10 +2,6 @@ use std::env;
 use std::path::Path;
 
 fn main() {
-    if env::var("CARGO_FEATURE_EMBED_ASSETS").is_ok() {
-        bevy_embedded_assets::embed_assets!();
-    }
-
     if env::var("CARGO_CFG_TARGET_OS").ok().as_deref() == Some("windows") {
         let mut res = winres::WindowsResource::new();
         // The path must be relative to the Cargo.toml file
