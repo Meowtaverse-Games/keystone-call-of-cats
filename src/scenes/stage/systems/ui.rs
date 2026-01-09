3c3f151d0ce56cb2462018fccb7b657db300e501
Tokusei Noborio <tokusei@nyampass.com>
2026-01-09 10:17:54 +0900

    style: Remove commented out debug logs

--- src/scenes/stage/systems/ui.rs
+++ src/scenes/stage/systems/ui.rs
@@ -758,15 +758,6 @@ pub fn tick_script_program(
             format!("is-empty-{}", name),
             ScriptStateValue::Bool(!is_blocked),
         );
-        // info!(
-        //     "is-empty-{}: {} (origin={:?}, radius={}, step={}, hit={:?})",
-        //     name,
-        //     !is_blocked,
-        //     origin,
-        //     collider_radius,
-        //     check_dist,
-        //     hit.map(|h| (h.distance, h.entity))
-        // );
     }
 
     if let Some(command) = program.next(&state) {
